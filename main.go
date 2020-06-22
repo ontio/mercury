4949777217d23c38bd780dd19ff982a6547c0cc1
JunXi Xie <xiemylogos@gmail.com>
2020-06-22 11:24:07 +0800

    add start server import wallet

--- main.go
+++ main.go
@@ -1,12 +1,44 @@
 package main
 
 import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
 	"github.com/gin-gonic/gin"
+	"git.ont.io/ontid/otf/utils"
 )
 
 func main() {
-
-	 := gin.Default()
+	r := gin.Default()
 	r.Use()
-	r.Run(":8080")
+	err := r.Run(":8080")
+	if err != nil {
+		panic(err)
+	}
+	account,err := utils.OpenAccount("./wallet.dat")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("addr:",account.Address)
+	signalHandle()
+}
+
+func signalHandle() {
+	var (
+		ch = make(chan os.Signal, 1)
+	)
+	signal.Notify(ch, syscall.SIGHUP, syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGINT)
+	for {
+		si := <-ch
+		switch si {
+		case syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGINT:
+			fmt.Println("get a signal: stop the rest gateway process", si.String())
+			return
+		case syscall.SIGHUP:
+		default:
+			return
+		}
+	}
 }
--- utils/utils.go
+++ utils/utils.go
@@ -1,3 +1,44 @@
 package utils
 
+import (
+	"fmt"
+	sdk "github.com/ontio/ontology-go-sdk"
+	"github.com/howeyc/gopass"
+)
+
 var Version = ""
+
+
+func OpenAccount(path string) (*sdk.Account,error){
+	ontSdk := sdk.NewOntologySdk()
+	wallet,err := ontSdk.OpenWallet("./wallet.dat")
+	if err != nil {
+		return nil,err
+	}
+	pwd, err := GetPassword()
+	if err != nil {
+		return nil,err
+	}
+	defer ClearPasswd(pwd)
+	account, err :=wallet.GetDefaultAccount(pwd)
+	if err != nil {
+		return nil,err
+	}
+	return account,nil
+}
+
+func GetPassword() ([]byte, error) {
+	fmt.Printf("Password:")
+	passwd, err := gopass.GetPasswd()
+	if err != nil {
+		return nil, err
+	}
+	return passwd, nil
+}
+
+func ClearPasswd(passwd []byte) {
+	size := len(passwd)
+	for i := 0; i < size; i++ {
+		passwd[i] = 0
+	}
+}
